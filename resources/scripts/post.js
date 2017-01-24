d55e44c35fa729a19369c62a6b41036f661afedd
Luciana Heuko <lucianaheuko@gmail.com>
2017-01-24 15:31:53 -0200

    layout

--- resources/scripts/post.js
+++ resources/scripts/post.js
@@ -1,19 +1,19 @@
 $(function () {
-  
+
   /**
    * Page header previews
    */
   var $previewContainer = $('.preview-container');
   var $previewIframes   = $previewContainer.find('iframe');
-  
+
   // add load event listener to iframes
   $previewIframes.on('load', function (e) {
     var $target = $(e.target);
     var $preview = $target.closest('.preview');
-    
+
     $preview.addClass('iframe-loaded');
   });
-  
+
   // delay iframe loading in order not to overload
   // browser rendering
   setTimeout(function () {
@@ -22,7 +22,7 @@ $(function () {
       $iframe.attr('src', $iframe.data('src'));
     });
   }, 500);
-  
+
   /**
    * Previewer (detailed)
    */
@@ -31,41 +31,42 @@ $(function () {
   var $previewerModeSelector = $previewer.find('#previewer-mode-selector');
   var $previewerClose        = $('[data-action="close-previewer"]');
   var $previewerOpen         = $('[data-action="open-previewer"]');
-    
+
   // load webpage
   $previewerIframe.attr('src', $previewerIframe.data('src'));
-  
+
   $previewer.open = function (mode) {
     if (!mode && !$previewer.attr('data-mode')) {
       $previewer.attr('data-mode', 'desktop');
     }
-    
+
     $previewer.addClass('active');
-    
+
     $('body').attr('data-previewer', 'active');
   };
-  
+
   $previewer.close = function () {
     $previewer.removeClass('active');
-    
+
     $('body').attr('data-previewer', 'inactive');
   };
-  
+
   // attach event handlers
   $previewerModeSelector.on('click', 'li', function (e) {
-    var mode = $(e.target).data('mode');
-    
+    console.log(e.currentTarget);
+    var mode = $(e.currentTarget).data('mode');
+
     if (mode) {
       $previewer.attr('data-mode', mode);
     }
   });
-  
+
   $previewerClose.on('click', function () {
     $previewer.close();
   });
-  
+
   $previewerOpen.on('click', function () {
     $previewer.open();
   });
-  
+
 });
