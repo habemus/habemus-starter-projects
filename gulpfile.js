3fc2e5780f2101025dac1d6cd0422394aab92112
Simon <simonxd@gmail.com>
2017-01-31 23:01:49 -0200

    restructured directories to include gulp building

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,23 @@
 const child_process = require('child_process');
+const path = require('path');
 
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 const gulpIf = require('gulp-if');
 const gulpCheerio = require('gulp-cheerio');
+const fse = require('fs-extra');
 
 function isCss(file) {
   return /\.css$/.test(file.path);
 }
 
-function isHtml(file) {
-  return /\.html$/.test(file.path);
+const HTML_RE = /\.html$/;
+const STARTER_PROJECTS_BASE_PATH = path.join(__dirname, 'src/_site/resources/starter-projects');
+function shouldHaveGA(file) {
+  return HTML_RE.test(file.path) &&
+        // do not include starter project's files into the GA,
+        // as they are loaded via iframe
+        !file.path.startsWith(STARTER_PROJECTS_BASE_PATH);
 }
 
 // HABEMUS.IO google analytics script
@@ -27,20 +34,26 @@ const GA_SCRIPT = `<script>
 
 gulp.task('distribute', function (done) {
 
-  child_process.execFile('jekyll', ['build'], (error, stdout, stderr) => {
+  var opts = {
+    cwd: path.join(__dirname, 'src'),
+  };
+
+  fse.removeSync(path.join(__dirname, 'dist'));
+
+  child_process.execFile('jekyll', ['build'], opts, (error, stdout, stderr) => {
 
     if (error) {
       throw error;
     }
-    gulp.src('_site/**/*')
+    gulp.src('src/_site/**/*')
+      // css
       .pipe(gulpIf(isCss, autoprefixer({
         browsers: ['last 2 versions'],
         cascade: false
       })))
-      .pipe(gulpIf(isHtml, gulpCheerio(function ($, file, done) {
-
+      // ga
+      .pipe(gulpIf(shouldHaveGA, gulpCheerio(function ($, file, done) {
         $('body').append(GA_SCRIPT);
-
         done();
       })))
       .pipe(gulp.dest('dist'))
